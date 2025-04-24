c82f64a731bd4270588daa0e0fdb8b0cc7774a70
asadAshrafi <47710506+asadAshrafi@users.noreply.github.com>
2025-04-24 19:08:00 +0500

    Initial commit
    Application structure

---
+++ Ecovite/Ecovite/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ecovite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecovite.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    }
+}
---
+++ Ecovite/Ecovite/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecovite.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public ICollection<OrderItem>? Items { get; set; }
+    }
+
+}
