a13681796f2ea205464bccaf8209fb8781448563
Asad <asad.ashrafee@gmail.com>
2025-04-27 21:20:49 +0500

    some changes

---
+++ Ecovite/Ecovite/Common/ApiException.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecovite.Common
+{
+    public class ApiException : Exception
+    {
+        public int ErrorCode { get; set; }
+        public string? ErrorId { get; set; }
+        public ApiException(int errorCode, string errorMessage,string? errorId = null):base(errorMessage) {
+        ErrorCode = errorCode;
+            ErrorId = errorId;
+        }
+    }
+}
--- Ecovite/Ecovite/obj/Debug/net8.0/Ecovite.AssemblyInfo.cs
+++ Ecovite/Ecovite/obj/Debug/net8.0/Ecovite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecovite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82f64a731bd4270588daa0e0fdb8b0cc7774a70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecovite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecovite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
